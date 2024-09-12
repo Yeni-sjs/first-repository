ca825c99858123f14ef509a3341e3f4f1cf25faf
Yeni-sjs <yg12300321@gmail.com>
2024-09-12 17:34:54 +0900

    HEYHEY

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,9 @@
 def printf_hello():
     print("Hllo World!!")
 
+def print_KIT():
+    print("Kumoh National Institute of Technology.")
+
 if __name__== '__main__':
+    print_KIT()
     print_hello()
